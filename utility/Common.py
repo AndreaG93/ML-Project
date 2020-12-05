43edc155199a953871981564871e17ff45333f91
Andrea Graziani <andrea.graziani93@outlook.it>
2020-12-05 16:21:25 +0100

    update

--- utility/Common.py
+++
@@ -1,7 +0,0 @@
-def show_shape(input_data, target_data):
-    """
-    An utility function to show 'shapes' of our CNN inputs.
-    """
-    print("Expected: (num_samples, timestamps, data)")
-    print(" Input Data -> {}".format(input_data.shape))
-    print("Target Data -> {}".format(target_data.shape))
